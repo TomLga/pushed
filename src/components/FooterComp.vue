5c466e2b73cecc2bdcdea4046a722b4fb4d8de88
Logan <129970580+TomLga@users.noreply.github.com>
2023-08-25 01:52:17 +0200

    Add files via upload
    home, nav, footer, admit table is coming from the other table

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="foot">
+        <p>CopyRight &copy; Frotend-Mongikazi Dlanjwas <br> 
+        <span>Backend-Logan Thomas</span></p>
+
+    </div>
+</template>
+
+
+<style>
+.foot{
+    background: #000000;
+    color: white;
+    text-align: center;
+  
+}
+
+
+</style>
